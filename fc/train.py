2b46ef7227d6b5259ae1d50cf3f8501acd0a9b50
fengyuwei <fengyuwei01@gmail.com>
2021-03-24 16:54:13 +0800

    update

--- fc/train.py
+++ fc/train.py
@@ -10,19 +10,19 @@ from network import *
 from sklearn.metrics import accuracy_score
 
 if __name__ == "__main__":
-    x, y = load_digits(return_X_y=True)
-    # x, y = fetch_openml('mnist_784', version=1, return_X_y=True)
+    #x, y = load_digits(return_X_y=True)
+    x, y = fetch_openml('mnist_784', version=1, return_X_y=True)
     print(x.shape)
     # print(y[0])
 
     y_oh = preprocessing.OneHotEncoder().fit_transform(y.reshape(-1, 1)).toarray()
     print(y.shape)
 
-    network = Network(64, [512, 256, 128], n_class=10)
-    network.train(x[0:1000], y_oh[0:1000], 100, 0.0001)
+    X_train, X_test, y_train, y_test = train_test_split(x, y_oh, test_size = 0.33, random_state = 42)
 
-    preds = network.predict(x[1001:1500])
-    y_true = y[1001:1500]
-    y_pred = np.argmax(preds, axis=1)
+    #network = Network(64, [128, 64, 32], n_class=10)
+    network = Network(784, [128, 64, 32], n_class=10)
+    network.train(X_train, y_train, 10, 0.0001)
 
-    print(accuracy_score(y_true, y_pred))
+    y_preds = network.predict(X_test)
+    print(accuracy_score(np.argmax(y_test, axis=1), np.argmax(y_preds, axis=1)))
